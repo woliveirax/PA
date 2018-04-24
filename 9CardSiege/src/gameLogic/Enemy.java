6abb6cc1dfb6552ae42d28b8c37507d34d969645
Joana <jbmaria.joana@gmail.com>
2018-04-24 21:51:12 +0100

    init Enemy & Day
    amo-te muito <3

---
+++ 9CardSiege/src/gameLogic/Enemy.java
@@ -0,0 +1,33 @@
+package gameLogic;
+
+import java.io.Serializable;
+
+
+public class Enemy implements Serializable{
+    GameData data;
+    int pos;
+    int strenght;
+
+    public Enemy(GameData data, int strenght) {
+        this.pos = 4;
+        this.data = data;
+        this.strenght = strenght;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+    
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+    
+}
