efb5faa351e98195be76aec2dd9057cc24d1d676
Unknown <wallace9655@gmail.com>
2018-06-17 13:29:15 +0100

    Resources finished

--- 9CardSiege/src/IU/GUI/Resources.java
+++ 9CardSiege/src/IU/GUI/Resources.java
@@ -1,6 +1,28 @@
 package IU.GUI;
 
 import IU.GUI.Constants.ImageConstants;
+import static IU.GUI.Constants.ImageConstants.path_backCard;
+import static IU.GUI.Constants.ImageConstants.path_background;
+import static IU.GUI.Constants.ImageConstants.path_card1;
+import static IU.GUI.Constants.ImageConstants.path_card2;
+import static IU.GUI.Constants.ImageConstants.path_card3;
+import static IU.GUI.Constants.ImageConstants.path_card4;
+import static IU.GUI.Constants.ImageConstants.path_card5;
+import static IU.GUI.Constants.ImageConstants.path_card6;
+import static IU.GUI.Constants.ImageConstants.path_card7;
+import static IU.GUI.Constants.ImageConstants.path_cardCastle;
+import static IU.GUI.Constants.ImageConstants.path_cardEnemies;
+import static IU.GUI.Constants.ImageConstants.path_dice1;
+import static IU.GUI.Constants.ImageConstants.path_dice2;
+import static IU.GUI.Constants.ImageConstants.path_dice3;
+import static IU.GUI.Constants.ImageConstants.path_dice4;
+import static IU.GUI.Constants.ImageConstants.path_dice5;
+import static IU.GUI.Constants.ImageConstants.path_dice6;
+import static IU.GUI.Constants.ImageConstants.path_loosePopUp;
+import static IU.GUI.Constants.ImageConstants.path_trebuchetsToken;
+import static IU.GUI.Constants.ImageConstants.path_tunnelPosToken;
+import static IU.GUI.Constants.ImageConstants.path_tunnelSuppliesToken;
+import static IU.GUI.Constants.ImageConstants.path_winPopUp;
 import java.awt.Image;
 import java.io.IOException;
 import java.net.URL;
@@ -12,7 +34,46 @@ public class Resources implements ImageConstants{
     
     private static List<Image> card_images = new ArrayList();
     private static List<Image> dice_faces = new ArrayList();
-        
+    private static Image background = null;
+    private static Image winPopUp, loosePopUp;
+    private static Image castleToken,tunnelSuppliesToken;
+    private static Image tunnelPosToken,trebuchetToken;
+    
+    
+    
+    //Load Background
+    static {
+        try {
+            background = ImageIO.read(Resources.getResourceFile(path_background));
+        } catch (IOException e) {
+            System.out.println("Error loading background");
+        }
+    }
+    
+    //Load end image
+    static {
+        try {
+            winPopUp = ImageIO.read(Resources.getResourceFile(path_winPopUp));
+            loosePopUp = ImageIO.read(Resources.getResourceFile(path_loosePopUp));
+        } catch (IOException e) {
+            System.out.println("Error loading background");
+        }
+    }
+    
+    //Load Tokens
+    static {
+        try {
+            castleToken         = ImageIO.read(Resources.getResourceFile(path_castleToken));
+            tunnelSuppliesToken = ImageIO.read(Resources.getResourceFile(path_tunnelSuppliesToken));
+            tunnelPosToken      = ImageIO.read(Resources.getResourceFile(path_tunnelPosToken));
+            trebuchetToken      = ImageIO.read(Resources.getResourceFile(path_trebuchetsToken));
+            
+        } catch (IOException e) {
+            System.out.println("Error loading tokens");
+        }
+    }
+    
+    //Load Dice Faces
     static {
         try {
             dice_faces.add(ImageIO.read(Resources.getResourceFile(path_dice1)));
@@ -21,6 +82,15 @@ public class Resources implements ImageConstants{
             dice_faces.add(ImageIO.read(Resources.getResourceFile(path_dice4)));
             dice_faces.add(ImageIO.read(Resources.getResourceFile(path_dice5)));
             dice_faces.add(ImageIO.read(Resources.getResourceFile(path_dice6)));
+            
+            } catch (IOException e) {
+                System.out.println("Error loading dice images");
+            }
+    }
+    
+    //Load Cards
+    static {
+        try {
             card_images.add(ImageIO.read(Resources.getResourceFile(path_card1)));
             card_images.add(ImageIO.read(Resources.getResourceFile(path_card2)));                                                                                                                    
             card_images.add(ImageIO.read(Resources.getResourceFile(path_card3)));                                                                                                                    
@@ -53,10 +123,37 @@ public class Resources implements ImageConstants{
         return dice_faces.get(pos);
     }
     
+    public static Image getBackground() {
+        return background;
+    }
+
+    public static Image getWinPopUp() {
+        return winPopUp;
+    }
+
+    public static Image getLoosePopUp() {
+        return loosePopUp;
+    }
+
+    public static Image getCastleToken() {
+        return castleToken;
+    }
+
+    public static Image getTunnelSuppliesToken() {
+        return tunnelSuppliesToken;
+    }
+
+    public static Image getTunnelPosToken() {
+        return tunnelPosToken;
+    }
+
+    public static Image getTrebuchetToken() {
+        return trebuchetToken;
+    }
+    
     public static final URL getResourceFile(String name){
             // opens a file with path relative to location of the Resources class
             URL url=Resources.class.getResource(name);
             return url; 
     }
-
 }
